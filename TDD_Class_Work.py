7358eeb08154ed7716d46230c5d2ce3f09b5fd60
idobe2 <idobe2@ac.sce.ac.il>
2022-11-20 20:52:59 +0200

    "TDD class work" tests updated

--- TDD_Class_Work.py
+++ TDD_Class_Work.py
@@ -1,19 +1,19 @@
 def testInput():
     text = input("Enter your text:\n")
-    if (text.isalpha() == True):
+    if text.isalpha():
         return text
     else:
         return False
 
 def textFix(text):
-    if (text.isalpha() == False):
+    if not text.isalpha():
         for i in range(len(text)):
             if 90 < ord(text[i]) < 97 or ord(text[i]) > 122:
                 text = text.replace(chr(ord(text[i])), chr(ord(text[i]) - 26))
         return text
 
 text = testInput()
-if (text):
+if text:
     for i in range(len(text)):
         text = text[:i] + chr(ord(text[i]) + 3) + text[i + 1:]
     text = textFix(text)
