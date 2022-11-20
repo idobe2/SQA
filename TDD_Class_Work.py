014f84b8f49709aa782a36aab10509137f6199bd
idobe2 <idobe2@ac.sce.ac.il>
2022-11-20 20:14:07 +0200

    "TDD class work" test added

--- TDD_Class_Work.py
+++ TDD_Class_Work.py
@@ -1,4 +1,11 @@
-text = input("Enter your text:\n")
-for i in range(len(text)):
-    text = text[:i] + chr(ord(text[i])+3) + text[i+1:]
-print(text)
+def testInput():
+    text = input("Enter your text:\n")
+    if (text.isalpha() == True):
+        return text
+    else: return False
+
+text = testInput()
+if (text):
+    for i in range(len(text)):
+        text = text[:i] + chr(ord(text[i])+3) + text[i+1:]
+    print(text)
