c9b2fdb7949c5ac57d6765619c1348283ed52c04
idobe2 <idobenoun@gmail.com>
2022-11-27 19:24:20 +0200

    BMI functions fixed

--- BMI_calc.py
+++ BMI_calc.py
@@ -1,47 +1,20 @@
 def test_input(height, weight):
+    """
+    :param height: (float) height in cm
+    :param weight: (float) weight in cm
+    :return: True/False
+    """
     if type(height) and type(weight) not in [int, float]:
         raise TypeError('The provided value is not a float')
-        return 0, 0
     else:
         if height <= 0 or weight <= 0:
             print("Invalid input")
-            return 0, 0
-        return height, weight
+            return False
+        return height, weight # True
 
 
 def bmi_calc():
-    """
-    סבב ראשון של הבדיקות:
-    height = float(input("Enter your height in cm: "))
-    weight = float(input("Enter your weight in kg: "))
-    bmi = weight / (height / 100) ** 2
-    print(f"Your bmi is {bmi}")
-    if bmi <= 18.5:
-        print("You are underweight.")
-    elif bmi <= 25:
-        print("You are healthy.")
-    else:
-        print("You are over weight.")
-        סבב שני של הבדיקות:
-    while True:
-        try:
-            height = float(input("Enter your height in cm: "))
-            weight = float(input("Enter your weight in kg: "))
-        except ValueError:
-            print('The provided value is not a float')
-            else:
-                break
-    test_input(height,weight)
-    bmi = weight / (height / 100) ** 2
-    print(f"Your bmi is {bmi}")
-    if bmi <= 18.5:
-        print("You are underweight.")
-    elif bmi <= 25:
-        print("You are healthy.")
-    else:
-        print("You are over weight.")
-    """
-    # הפונקציה לאחר תיקונים:
+
     while True:
         try:
             height = float(input("Enter your height in cm: "))
@@ -49,20 +22,33 @@ def bmi_calc():
             if (height <= 0) or (weight <= 0):
                 print("Value can not be zero or negative")
         except ValueError:
-            print('The provided value is not a float')
+            print("The provided value is not a float")
         else:
             if (height > 0) and (weight > 0):
                 break
-    test_input(height,weight)
-    bmi = weight / (height / 100) ** 2
-    print(f"Your bmi is {bmi}")
-    if bmi <= 18.5:
-        print("You are underweight.")
-    elif bmi <= 25:
-        print("You are healthy.")
-    else:
-        print("You are over weight.")
-
+    if test_input(height, weight):
+        bmi = weight / (height / 100) ** 2
+        print(f"Your bmi is {bmi}")
+        if bmi <= 18.5:
+            print("You are underweight.")
+        elif bmi <= 25:
+            print("You are healthy.")
+        else:
+            print("You are over weight.")
 
 
 bmi_calc()
+
+"""
+Round 1 of tests: (input)
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+bmi = weight / (height / 100) ** 2
+print(f"Your bmi is {bmi}")
+if bmi <= 18.5:
+    print("You are underweight.")
+elif bmi <= 25:
+    print("You are healthy.")
+else:
+    print("You are over weight.")
+"""
